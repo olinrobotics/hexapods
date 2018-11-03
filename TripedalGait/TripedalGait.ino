202ddfe3c127e9c8e909a0c2c013056709f3dc8d
Paul <paul.nadan@students.olin.edu>
2018-11-02 20:07:26 -0400

    Fixed Arduino version compatibility issues

--- TripedalGait/TripedalGait.ino
+++ TripedalGait/TripedalGait.ino
@@ -5,10 +5,10 @@ Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver();
 Adafruit_PWMServoDriver pwm2 = Adafruit_PWMServoDriver(0x41);
 
 // Gait parameters
-int ground = -5; // Height of ground relative to body (in)
-int clearance = 2; // Height of raised leg relative to body (in)
-int dx = 2.5; // Half of the total step distance (in)
-int yoffset = 5; // Horizontal distance of feet from body (in)
+float ground = -5; // Height of ground relative to body (in)
+float clearance = 2; // Height of raised leg relative to body (in)
+float dx = 2.5; // Half of the total step distance (in)
+float yoffset = 5; // Horizontal distance of feet from body (in)
 int stepDuration = 250; // Time duration of a step in (ms)
 
 // Pins
@@ -104,29 +104,30 @@ void moveLegToState(int leg, int state) {
 // Legs numbered 1-6 CCW from front left
 // States numbered 1-4 back-up-forward-down
 void getLegPosition(int leg, int state, float* output) {
-    float pos[] = {R*cos(leg*M_PI/3-M_PI/6),R*sin(leg*M_PI/3-M_PI/6),ground};
-    if(state == 2) { // leg lifted
-        pos[2] += clearance;
-    }
-    if (leg <=3) { // left side
-        pos[1] += yoffset;
-    } else {
-        pos[1] -= yoffset;
-    }
-    if (state == 1) { // leg back
-        pos[0] -= dx;
-    }
-    else if (state == 3) { // leg forward
-        pos[0] += dx;
-    }
-    for(int i=0; i<3; i++) {
-      output[i] = pos[i];
-    }
+  float pos[] = {R*cos(leg*M_PI/3-M_PI/6),R*sin(leg*M_PI/3-M_PI/6),ground};
+  if(state == 2) { // leg lifted
+    pos[2] += clearance;
+  }
+  if (leg <=3) { // left side
+    pos[1] += yoffset;
+  } else {
+    pos[1] -= yoffset;
+  }
+  if (state == 1) { // leg back
+    pos[0] -= dx;
+  }
+  else if (state == 3) { // leg forward
+    pos[0] += dx;
+  }
+  for(int i=0; i<3; i++) {
+    output[i] = pos[i];
+  }
 }
 
 // Move all 3 servos of a leg to position the end effector
 void moveLegToPosition(float x, float y, float z, int leg) {
-  int *angles = getAngles(x, y, z, leg);
+  int angles[3];
+  getAngles(x, y, z, leg, angles);
   if(angles[0] != -1) {
     moveLeg(angles, leg);
   }
@@ -134,52 +135,52 @@ void moveLegToPosition(float x, float y, float z, int leg) {
 
 // Determine servo angles (deg) from position relative to hexapod center (in)
 // x: forward, y: left, z: up, leg: CCW starting with front left leg
-int getAngles(float x, float y, float z, int leg) {
-    int error[] = {-1, -1, -1};
-    float a = 0;
-    float b = 0;
-    float c = 0;
-    x = x - R*cos(leg*M_PI/3 - M_PI/6);
-    y = y - R*sin(leg*M_PI/3 - M_PI/6);
-    a = atan2(y, x) + M_PI/6 - leg*M_PI/3;
-    float r = sqrt(pow(x,2)+pow(y,2)) - L1;
-    if (a < -M_PI) {
-        a = a + 2*M_PI;
+void getAngles(float x, float y, float z, int leg, int* angles) {
+  float a = 0;
+  float b = 0;
+  float c = 0;
+  x = x - R*cos(leg*M_PI/3 - M_PI/6);
+  y = y - R*sin(leg*M_PI/3 - M_PI/6);
+  a = atan2(y, x) + M_PI/6 - leg*M_PI/3;
+  float r = sqrt(pow(x,2)+pow(y,2)) - L1;
+  if (a < -M_PI) {
+      a = a + 2*M_PI;
+  }
+  if (a < -M_PI/2) {
+      a = a + M_PI;
+      r = -sqrt(pow(x,2)+pow(y,2)) - L1;
+  }
+  if (a > M_PI/2) {
+      a = a - M_PI;
+      r = -sqrt(pow(x,2)+pow(y,2)) - L1;
+  }
+  if (r > L2 + L3 || pow(r,2)+pow(z,2) == 0) {
+      Serial.println("Requested configuration out of reach");
+      angles[0] = angles[1] = angles[2] = -1;
+      return;
+  }
+  float r1 = (pow(r,3)+r*pow(z,2)+r*pow(L2,2)-r*pow(L3,2)-z*sqrt(-pow(r,4)-pow(z,4)-pow((pow(L2,2)-pow(L3,2)),2)+2*pow(z,2)*
+      (pow(L2,2)+pow(L3,2))+2*pow(r,2)*(-pow(z,2)+pow(L2,2)+pow(L3,2))))/(2*(pow(r,2)+pow(z,2)));
+  float z1 = (pow(z,3)+z*pow(r,2)+z*pow(L2,2)-z*pow(L3,2)+r*sqrt(-pow(r,4)-pow(z,4)-pow((pow(L2,2)-pow(L3,2)),2)+2*pow(z,2)*
+      (pow(L2,2)+pow(L3,2))+2*pow(r,2)*(-pow(z,2)+pow(L2,2)+pow(L3,2))))/(2*(pow(r,2)+pow(z,2)));
+  if (isnan(r1)|| isnan(z1)) {
+      Serial.println("Requested configuration has no solution");
+      angles[0] = angles[1] = angles[2] = -1;
+      return;
+  }
+  b = -atan2(z1, r1);
+  c = -atan2(z-z1, r-r1) - b;
+  angles[0] = round(180/M_PI*a + 90);
+  angles[1] = round(-180/M_PI*b + 90);
+  angles[2] = round(180/M_PI*c);
+  for(int i=0; i<3; i++) {
+    if (angles[i] > maxLimits[i] || angles[i] < minLimits[i]) {
+      Serial.print("Requested configuration outside servo range: Servo ");
+      Serial.println(i);
+      angles[0] = angles[1] = angles[2] = -1;
+      return;
     }
-    if (a < -M_PI/2) {
-        a = a + M_PI;
-        r = -sqrt(pow(x,2)+pow(y,2)) - L1;
-    }
-    if (a > M_PI/2) {
-        a = a - M_PI;
-        r = -sqrt(pow(x,2)+pow(y,2)) - L1;
-    }
-    if (r > L2 + L3 || pow(r,2)+pow(z,2) == 0) {
-        Serial.println("Requested configuration out of reach");
-        return(error);
-    }
-    float r1 = (pow(r,3)+r*pow(z,2)+r*pow(L2,2)-r*pow(L3,2)-z*sqrt(-pow(r,4)-pow(z,4)-pow((pow(L2,2)-pow(L3,2)),2)+2*pow(z,2)*
-        (pow(L2,2)+pow(L3,2))+2*pow(r,2)*(-pow(z,2)+pow(L2,2)+pow(L3,2))))/(2*(pow(r,2)+pow(z,2)));
-    float z1 = (pow(z,3)+z*pow(r,2)+z*pow(L2,2)-z*pow(L3,2)+r*sqrt(-pow(r,4)-pow(z,4)-pow((pow(L2,2)-pow(L3,2)),2)+2*pow(z,2)*
-        (pow(L2,2)+pow(L3,2))+2*pow(r,2)*(-pow(z,2)+pow(L2,2)+pow(L3,2))))/(2*(pow(r,2)+pow(z,2)));
-    if (isnan(r1)|| isnan(z1)) {
-        Serial.println("Requested configuration has no solution");
-        return(error);
-    }
-    b = -atan2(z1, r1);
-    c = -atan2(z-z1, r-r1) - b;
-    a = 180/M_PI*a + 90;
-    b = -180/M_PI*b + 90;
-    c = 180/M_PI*c;
-    int angles[] = {round(a), round(b), round(c)};
-    for(int i=0; i<3; i++) {
-      if (angles[i] > maxLimits[i] || angles[i] < minLimits[i]) {
-        Serial.print("Requested configuration outside servo range: Servo ");
-        Serial.println(i);
-        return(error);
-      }
-    }
-    return(angles);
+  }
 }
 
 // Move all 3 servos of a leg to a given state
